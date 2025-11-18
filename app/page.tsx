a0b64878b20ff6acb8019931beb5e03ec84f7eb7
vipendra <vipendrasinghpatel999@gmail.com>
2025-11-18 22:27:00 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read Documentation
+            Documentation
           </a>
         </div>
       </main>
