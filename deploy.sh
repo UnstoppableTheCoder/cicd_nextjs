3b9861b9b6aae4e380c36a5c2c3d8e2b77d98d57
vipendra <vipendrasinghpatel999@gmail.com>
2025-11-18 22:44:37 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+export NVM_DIR="$HOME/.nvm"
+source $NVM_DIR/nvm.sh
+nvm use 20.9.0
+cd cicd_nextjs
+git pull origin main
+npm install
+npm run build
+npm run start
 cd cicd_nextjs
 git pull origin main
 npm install
